6f4eb1193eb2bf16a6df12a4345f1c0639b75e69
Panpan <pcai2@e.ntu.edu.sg>
2017-09-20 17:35:12 +0800

    Git track added files

---
+++ include/despot/core/pomdp_world.h
@@ -0,0 +1,48 @@
+/*
+ * POMDPWorld.h
+ *
+ *  Created on: 20 Sep 2017
+ *      Author: panpan
+ */
+
+#ifndef INCLUDE_DESPOT_CORE_POMDP_WORLD_H_
+#define INCLUDE_DESPOT_CORE_POMDP_WORLD_H_
+
+#include <despot/core/globals.h>
+#include <despot/core/pomdp.h>
+#include <despot/core/world.h>
+#include <despot/util/random.h>
+
+namespace despot {
+
+class POMDPWorld: public World {
+protected:
+    //POMDP model shared with despot solver
+    DSPOMDP* model_;
+    //Random number generator.
+    Random random_;
+public:
+    double step_reward_;
+
+public:
+    POMDPWorld(DSPOMDP* model, unsigned seed);
+    virtual ~POMDPWorld();
+	virtual inline void world_seed(unsigned seed) {
+		random_ = Random(seed);
+	}
+
+public:
+   //establish connection to simulator or system
+	bool Connect();
+   //Initialize or reset the simulation environment
+	State* Initialize();
+   //To help construct initial belief to print debug informations in Evaluator
+	State* GetTrueState() const;
+   //send action, receive reward, obs, and terminal
+	bool ExecuteAction(int action, OBS_TYPE& obs);
+};
+
+
+} /* namespace despot */
+
+#endif /* INCLUDE_DESPOT_CORE_POMDP_WORLD_H_ */
---
+++ src/core/pomdp_world.cpp
@@ -0,0 +1,40 @@
+/*
+ * POMDPWorld.cpp
+ *
+ *  Created on: 20 Sep 2017
+ *      Author: panpan
+ */
+
+#include <despot/core/pomdp_world.h>
+
+namespace despot {
+
+POMDPWorld::POMDPWorld(DSPOMDP* model, unsigned seed):
+	model_(model){
+	random_ = Random(seed);
+}
+
+POMDPWorld::~POMDPWorld() {
+}
+
+
+bool POMDPWorld::Connect() {
+	return true;
+}
+
+State* POMDPWorld::Initialize() {
+	state_ = model_->CreateStartState();
+	return state_;
+}
+
+despot::State* POMDPWorld::GetTrueState() const {
+	return state_;
+}
+
+bool POMDPWorld::ExecuteAction(int action, OBS_TYPE& obs) {
+	bool terminal = model_->Step(*state_, random_.NextDouble(), action,
+			step_reward_, obs);
+	return terminal;
+}
+
+} /* namespace despot */
